607e5a6b2d1f7cc1a63f738b4fc111ae1c0eb6a6
JohnVy <contact@spiritvy.com>
2024-09-15 13:24:37 +0200

    fix dashboards view

--- app/controllers/admin/dashboard_controller.ts
+++ app/controllers/admin/dashboard_controller.ts
@@ -1,7 +1,24 @@
+import { UserRole } from '#enums/user_role'
+import Customer from '#models/customer'
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class DashboardController {
-  render({ inertia }: HttpContext) {
-    return inertia.render('admin/dashboard')
+  async show({ auth, inertia, response }: HttpContext) {
+    const user = auth.use('web').user!
+
+    if (user.role !== UserRole.Admin) {
+      return response.redirect().toRoute('/login')
+    }
+
+    const customerCountResult = await Customer.query().count('* as total').first()
+    const customerCount = customerCountResult || 0
+
+    const recentCustomers = await Customer.query().orderBy('created_at', 'desc').limit(5)
+
+    return inertia.render('admin/dashboard', {
+      user,
+      customerCount,
+      recentCustomers,
+    })
   }
 }
--- app/controllers/client/dashboard_controller.ts
+++ app/controllers/client/dashboard_controller.ts
@@ -1,7 +1,19 @@
+import UserProfile from '#models/user_profile'
 import { HttpContext } from '@adonisjs/core/http'
 
 export default class DashboardController {
-  render({ inertia }: HttpContext) {
-    return inertia.render('client/dashboard')
+  async show({ auth, inertia }: HttpContext) {
+    const user = auth.use('web').user!
+
+    // Récupération du profil utilisateur
+    const profile = await UserProfile.query().where('user_id', user.id).first()
+
+    // Récupération des informations client, si disponibles
+    // const customer = await Customer.query().where('user_id', user.id).first()
+
+    return inertia.render('client/dashboard', {
+      user,
+      profile,
+    })
   }
 }
